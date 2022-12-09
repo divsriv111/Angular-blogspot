71d6f757f2a9e3a7e77f06c2068e7a9c9bd569c6
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2022-12-10 01:21:51 +0530

    added env files

---
+++ ang-blog-dashboard/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC4oDXcOVRyQC6iL4oUaTcyl3vylc_aYpQ',
+    authDomain: 'ang-blog-5c821.firebaseapp.com',
+    projectId: 'ang-blog-5c821',
+    storageBucket: 'ang-blog-5c821.appspot.com',
+    messagingSenderId: '442224413919',
+    appId: '1:442224413919:web:ede18e27c35db4cba92d6a',
+    measurementId: 'G-XMF2WT631Z',
+  },
+};
